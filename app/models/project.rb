156227c1dbdc2f0c712d17e82d0a8ac41d7344b5
vasyl.marmash <vasyl.marmash@teamvoy.com>
2022-07-16 13:07:20 +0300

    add dependent destroy

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,7 +1,7 @@
 class Project < ApplicationRecord
   validates :name, presence: true, length: { minimum: 3 }
 
-  has_many :user_projects
+  has_many :user_projects, dependent: :destroy
   has_many :users, through: :user_projects
-  has_many :costs
+  has_many :costs, dependent: :destroy
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,6 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 has_many :tasks, dependent: :destroy
-has_many :user_projects
+has_many :user_projects, dependent: :destroy
 has_many :projects, through: :user_projects
 end
