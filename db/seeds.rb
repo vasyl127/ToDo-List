1be19c9c82f1783d2b7b66559264fd14bca9dc79
vasyl.marmash <vasyl.marmash@teamvoy.com>
2022-07-18 21:56:51 +0300

    add project controller and view and routes

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,12 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+User.all.each do |user|
+  20.times do |i|
+    project = Project.create(name: "Prject ##{i}")
+    5.times do |k|
+      project.costs.create(name: "cost ##{k}", title: k * 100)
+    end
+    UserProject.create(user_id: user.id, project_id: project.id)
+  end
+end
